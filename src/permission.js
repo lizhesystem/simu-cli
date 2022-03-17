b11fcbc2a67b0dc621b015a1ad03f8950aaa8d49
Administrator <lizhesystem@126.com>
2022-03-17 20:36:42 +0800

    新增electron 打包配置

--- src/permission.js
+++ src/permission.js
@@ -66,9 +66,9 @@ router.beforeEach((to, from, next) => {
 
 router.afterEach(() => {
   NProgress.done();
-  let title = store.getters.tag.label;
-  let i18n = store.getters.tag.meta.i18n;
-  title = router.$avueRouter.generateTitle(title, i18n)
+  // let title = store.getters.tag.label;
+  // let i18n = store.getters.tag.meta.i18n;
+  // title = router.$avueRouter.generateTitle(title, i18n)
   //根据当前的标签也获取label的值动态设置浏览器标题
-  router.$avueRouter.setTitle(title);
+  // router.$avueRouter.setTitle(title);
 });
